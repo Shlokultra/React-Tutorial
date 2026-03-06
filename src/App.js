92fffcb5b4b7eb37e77c24ab417fddc17e010763
Shlok Jitendra Yadav <yadavshlok2005@gmail.com>
2026-03-06 14:13:41 +0530

    cwh video10 half

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+// import Textform from './components/Textform';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtills" aboutText="About TextUtills" homeText="Home TextUtills"/>
       <div className="container my-3">
-      <Textform  heading="Enter the text to analyze"/>
+        <About />
+      {/* <Textform  heading="Enter the text to analyze"/> */}
       </div>
     </>
   );
